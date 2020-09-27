d49bca5b82656cbb0b169761ec65ee1968c3db8e
ramosv <56368263+ramosv@users.noreply.github.com>
2020-09-27 13:37:00 -0600

    #3

--- eshopping/eshop/views.py
+++ eshopping/eshop/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
+from .models import *
 
 def store(request):
-    contex = {}
+    products = Product.objects.all()
+    contex = {'products':products}
     return render(request, 'store.html', contex)
 
 def cart(request):
-    contex = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    contex = {'items':items, 'order':order}
     return render(request, 'cart.html', contex)
 
 def checkout(request):
-    contex = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    contex = {'items':items, 'order':order}
     return render(request, 'checkout.html', contex)
 
 
