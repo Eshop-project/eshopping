8ec0c5d77687be410dbc25db5c7bea24dc519e45
ramosv <56368263+ramosv@users.noreply.github.com>
2020-10-16 20:16:23 -0600

    Guest Checkout

--- eshopping/eshop/views.py
+++ eshopping/eshop/views.py
@@ -3,50 +3,112 @@ from django.http import JsonResponse
 import json
 import datetime
 from .models import *
+from . utils import cookieCart, cartData, guestOrder
 
 def store(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
+    """
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
+
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
-        cartItems = order['get_cart_items']
+        cartItems = order['get_cart_items']"""
 
     products = Product.objects.all()
     contex = {'products':products, 'cartItems': cartItems}
     return render(request, 'store.html', contex)
 
 def cart(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
-    if request.user.is_authenticated:
+    """if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
+
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart= {}
+        print('Cart:', cart)
+
+        
+        
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
         cartItems = order['get_cart_items']
+        for x in cart:
+            try:
+                cartItems += cart[x]["quantity"]
+                product = Product.objects.get(id=x)
+                total = (product.price * cart[x]["quantity"])
 
-    contex = {'items':items, 'order':order, 'cartItems': cartItems, 'shipping':False}
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[x]['quantity']
+                item ={
+                    'product':{
+                        'id':product.id,
+                        'name':product.name,
+                        'price':product.price,
+                        'imageURL':product.imageURL,
+                        },
+                    'quantity':cart[x]['quantity'],
+                    'get_total':total
+
+                }
+                items.append(item)
+
+                if product.digital == False:
+                    order['shipping'] = True
+            except:
+                pass """
+
+    contex = {'items':items, 'order' :order, 'cartItems':cartItems}
     return render(request, 'cart.html', contex)
 
 def checkout(request):
-    if request.user.is_authenticated:
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+    """if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
+
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
-        cartItems = order['get_cart_items']
+        cartItems = order['get_cart_items']"""
 
-    contex = {'items':items, 'order':order, 'cartItems': cartItems, 'shipping':False}
+    contex = {'items':items, 'order':order, 'cartItems': cartItems}
     return render(request, 'checkout.html', contex)
 
 
@@ -83,6 +145,7 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        """
         total = float(data['form']['total'])
         order.transaction_id = transaction_id
 
@@ -98,11 +161,57 @@ def processOrder(request):
                 city=data['shipping']['city'],
                 state=data['shipping']['state'],
                 zipcode=data['shipping']['zipcode'],
-            )
+            )"""
     
     
     else:
+        customer, order = guestOrder(request, data)
+        """
         print('User is Not logged in... ')
+        print('COOkIES:', request.COOKIES)
+        name = data['form']['name']
+        email = data['form']['email']
+        
+        cookieData = cookieCart(request)
+        items = cookieData['items']
+        
+        customer, created = Customer.objects.get_or_create(
+            email = email,
+        )
+        
+        customer.name = name
+        customer.save()
+
+        order = Order.objects.create(
+            customer = customer,
+            complete = False,
+        )
+        
+        for item in items:
+            product = Product.objects.get(id=item['product']['id'])
+
+            orderItem = OrderItem.objects.create(
+                product=product,
+                order=order,
+                quantity=item['quantity']
+            )"""
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
     return JsonResponse('Payment Complete!', safe=False)
 
 
